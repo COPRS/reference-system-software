726d01def966aabc543b79f63eeb2440c9478c55
nleconte-csgroup <86782407+nleconte-csgroup@users.noreply.github.com>
2021-09-06 09:56:31 +0200

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ class UneClasseDeTest(unittest.TestCase):
 
     def test_addition(self):
         result = app.addition(1,1)
-        self.assertEqual(result,3)
+        self.assertEqual(result,2)
 
 if __name__ == '__main__':
     unittest.main()
